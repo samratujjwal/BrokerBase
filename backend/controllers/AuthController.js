4696c459d6e2df50a9df16f5ccb5a547d801b4e5
samratujjwal <ujjawalmaurya45@gmail.com>
2026-01-23 17:53:27 +0530

    Initial commit

---
+++ backend/controllers/AuthController.js
@@ -0,0 +1,61 @@
+const User = require("../model/UserModel");
+const { createSecretToken } = require("../util/SecretToken");
+const bcrypt = require("bcryptjs");
+
+module.exports.Signup = async (req, res, next) => {
+    try {
+        const { email, password, username, createdAt } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.json({ message: "User already exists" });
+        }
+        const user = await User.create({ email, password, username, createdAt });
+        const token = createSecretToken(user._id);
+        //console.log("Generated token for signup:", token);
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None",
+            maxAge: 24 * 60 * 60 * 1000, // 24 hours
+            path: "/"
+        });
+        res
+            .status(201)
+            .json({ message: "User signed in successfully", success: true, user });
+        next();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+module.exports.Login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.json({ message: 'All fields are required' })
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({ message: 'Incorrect password or email' })
+        }
+        const auth = await bcrypt.compare(password, user.password)
+        if (!auth) {
+            return res.json({ message: 'Incorrect password or email' })
+        }
+        const token = createSecretToken(user._id);
+        //console.log("Generated token for login:", token);
+        //console.log("User found:", user.username);
+        res.cookie("token", token, {
+            //withCredentials: true,
+            httpOnly: true,
+            secure: true,
+            maxAge: 24 * 60 * 60 * 1000,
+            path: "/"
+        });
+        res.status(201).json({ message: "User logged in successfully", success: true, user: user.username });
+        next()
+    } catch (error) {
+        console.log("Some error Occurred!");
+        console.error(error);
+    }
+}
---
+++ frontend/src/landing_page/signup/Login.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+function Login({ onLoginSuccess }) {
+    const navigate = useNavigate();
+    const [inputValue, setInputValue] = useState({
+        email: "",
+        password: "",
+    });
+    const { email, password } = inputValue;
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue({
+            ...inputValue,
+            [name]: value,
+        });
+    };
+
+    const handleError = (err) =>
+        toast.error(err, {
+            position: "bottom-left",
+        });
+    const handleSuccess = (msg) =>
+        toast.success(msg, {
+            position: "top-right",
+        });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.post(
+                //"https://brokerbase.onrender.com/login",
+                "http://localhost:3002/login",
+                {
+                    ...inputValue,
+                },
+                { withCredentials: true }
+            );
+            //console.log(data);
+            const { success, message } = data;
+            if (success) {
+                handleSuccess(message);
+                onLoginSuccess();
+                setTimeout(() => {
+                    navigate("/signup");
+                }, 1000);
+                setInputValue({
+                    email: "",
+                    password: "",
+                });
+                // console.log("About to call onLoginSuccess");
+                // console.log("Document cookies after login:", document.cookie);
+                onLoginSuccess();
+                console.log(" called onLoginSuccess");
+            } else {
+                handleError(message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
+    return (
+        <>
+            <form onSubmit={handleSubmit} style={{ width: "80%" }}>
+                <div className="mb-3">
+                    <label htmlFor="loginEmail" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="loginEmail"
+                        name="email"
+                        value={email}
+                        placeholder="Enter your email"
+                        onChange={handleOnChange}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="loginPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="loginPassword"
+                        name="password"
+                        value={password}
+                        placeholder="Enter your password"
+                        onChange={handleOnChange}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary w-100 mb-3">Login</button>
+            </form>
+            <ToastContainer />
+        </>
+    );
+}
+
+export default Login;
