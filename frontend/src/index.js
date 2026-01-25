c20bbeaeb3a88cddcdf5765bae5678b9cbe46dc7
samratujjwal <ujjawalmaurya45@gmail.com>
2026-01-25 18:18:20 +0530

    href link error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,21 +13,6 @@ import Footer from "./landing_page/Footer.js";
 import NotFound from "./landing_page/NotFound.js";
 import { CookiesProvider } from "react-cookie";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import HomePage from "./landing_page/home/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./landing_page/signup/SignupPage.js";
-import AboutPage from "./landing_page/about/AboutPage.js";
-import PricingPage from "./landing_page/pricing/PricingPage.js";
-import ProductsPage from "./landing_page/products/ProductsPage.js";
-import SupportPage from "./landing_page/support/SupportPage.js";
-import Navbar from "./landing_page/Navbar.js";
-import Footer from "./landing_page/Footer.js";
-import NotFound from "./landing_page/NotFound.js";
-import { CookiesProvider } from "react-cookie";
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CookiesProvider>
     <BrowserRouter>
