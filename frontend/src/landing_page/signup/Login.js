2d0ff61e3d640351ef4fe1b7f3f779fe639b22ce
UJJWAL MAURYA <154653092+samratujjwal@users.noreply.github.com>
2026-01-26 21:50:32 +0530

    Remove unused Link import from Login.js

--- frontend/src/landing_page/signup/Login.js
+++ frontend/src/landing_page/signup/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
 import { ToastContainer, toast } from "react-toastify";
 function Login({ onLoginSuccess }) {
